3854e119a389ef8dc6f9e1611876911957546dad
Agung Kurniawan <agung.kurniawan@bsgroup.id>
2022-11-21 13:55:42 +0700

    Simpan dan Menampilkan Gambar - SQL Server

--- SimpanGambar/Form1.Designer.cs
+++ SimpanGambar/Form1.Designer.cs
@@ -28,13 +28,85 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnSelect = new System.Windows.Forms.Button();
+            this.textBox = new System.Windows.Forms.TextBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.btnLoad = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnSelect
+            // 
+            this.btnSelect.Location = new System.Drawing.Point(209, 304);
+            this.btnSelect.Name = "btnSelect";
+            this.btnSelect.Size = new System.Drawing.Size(75, 23);
+            this.btnSelect.TabIndex = 0;
+            this.btnSelect.Text = "Select";
+            this.btnSelect.UseVisualStyleBackColor = true;
+            this.btnSelect.Click += new System.EventHandler(this.btnSelect_Click);
+            // 
+            // textBox
+            // 
+            this.textBox.Location = new System.Drawing.Point(12, 306);
+            this.textBox.Name = "textBox";
+            this.textBox.Size = new System.Drawing.Size(191, 20);
+            this.textBox.TabIndex = 1;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(12, 333);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(272, 23);
+            this.btnSave.TabIndex = 2;
+            this.btnSave.Text = "Simpan Gambar";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(272, 286);
+            this.pictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox.TabIndex = 3;
+            this.pictureBox.TabStop = false;
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Location = new System.Drawing.Point(12, 362);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(272, 23);
+            this.btnLoad.TabIndex = 4;
+            this.btnLoad.Text = "Tampilkan Gambar";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(295, 394);
+            this.Controls.Add(this.btnLoad);
+            this.Controls.Add(this.pictureBox);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.textBox);
+            this.Controls.Add(this.btnSelect);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnSelect;
+        private System.Windows.Forms.TextBox textBox;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.Button btnLoad;
     }
 }
 
--- SimpanGambar/Form1.cs
+++ SimpanGambar/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
@@ -14,6 +15,78 @@ namespace SimpanGambar
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btnSelect_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog open = new OpenFileDialog();
+            open.Filter = "Image Files(*.jpg; *.jpeg; *.bmp; *.png; *.ico) | *.jpg; *.jpeg; *.bmp; *.png; *.ico";
+            if(open.ShowDialog() == DialogResult.OK)
+            {
+                pictureBox.Image = new Bitmap(open.FileName);
+                textBox.Text = open.FileName;
+            }
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            if(textBox.Text=="")
+            {
+                MessageBox.Show("Pilih gambar terlebih dahulu");
+            }
+            else
+            {
+                string name = textBox.Text;
+                string[] strpath = name.Split(Convert.ToChar(@"."));
+
+                MemoryStream ms = new MemoryStream();
+                pictureBox.Image.Save(ms, System.Drawing.Imaging.ImageFormat.Png);
+                int fileLenght = Convert.ToInt32(ms.Length) / 1024;
+
+                try
+                {
+                    Data.Connect();
+                    Data.Command("INSERT INTO SimpanGambar(Name,Type,Size,Data,DateUpload) VALUES(@0,@1,@2,@3,GETDATE())",
+                        new object[] { name, strpath[strpath.Length - 1], fileLenght, ms.ToArray() });
+
+                    MessageBox.Show("Gambar berhasil disimpan");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.ToString(),"GAGAL");
+                }
+                finally
+                {
+                    Data.Disconnect();
+                }
+            }
+        }
+
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Data.Connect();
+                DataTable dt = Data.SelectDataTable("SELECT Data FROM SimpanGambar", null);
+                if(dt!=null)
+                {
+                    if(dt.Rows.Count>0)
+                    {
+                        Byte[] data = new byte[0];
+                        data = (Byte[])(dt.Rows[0][0]);
+                        MemoryStream ms = new MemoryStream(data);
+                        pictureBox.Image = Image.FromStream(ms);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString(), "GAGAL");
+            }
+            finally
+            {
+                Data.Disconnect();
+            }
         }
     }
 }
